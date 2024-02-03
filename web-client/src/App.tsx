814c31ba5571acb0dc862c6d93ce1f78bf3bbb78
Harsh Shelar <harshshelar22@gmail.com>
2024-02-03 18:45:34 -0500

    screens implemented

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dashboard from './dashboard';
 import StressForm from './dashboard/stress-form';
-// import ChatBotComponent from './Chatbot/ChatBotComponent';
+import ChatBotComponent from './Chatbot/ChatBotComponent';
+
+type Screen = 'form' | 'chat';
 
 function App() {
+
+  const [screen, setScreen] = useState<Screen>('form');
+
+  const changeScreen = (newScreen: Screen) => {
+    setScreen(newScreen);
+  }
+
   return (
     <div style={{
       display: "flex",
       flexDirection: "row",
       justifyContent: "center",
     }}>
-      <Dashboard>
-        <StressForm />
+      <Dashboard changeScreen={changeScreen}>
+        {
+          screen === 'form' ? <StressForm /> : <></>
+        }
+        {
+          screen === 'chat' ? <ChatBotComponent /> : <></>
+        }
       </Dashboard>
-      {/* <ChatBotComponent/> */}
     </div >
   );
 }
