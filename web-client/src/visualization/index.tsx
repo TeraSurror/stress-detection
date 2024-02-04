4a1224bcde57e3b086634ab5791d05f9bf57eaa2
Harsh Shelar <harshshelar22@gmail.com>
2024-02-04 07:30:09 -0500

    visuals added

--- web-client/src/visualization/index.tsx
+++ web-client/src/visualization/index.tsx
@@ -2,42 +2,80 @@ import React from "react"
 import { Chart } from "react-google-charts";
 
 import sleepDurationData from '../data/sleep_duration.json';
-
+import dailyStepsData from '../data/daily_steps.json';
+import bpHigh from '../data/bp_high.json';
+import bpLow from '../data/bp_low.json';
 
 const Visualization: React.FC = () => {
 
-    const sleepDurationObj = {}
-    const sleepDurationList: string[][] = []
+    const getDistribution = (data: any, title: string) => {
+        const dataObj = {}
+        const dataList: string[][] = []
 
-    Object.keys(sleepDurationData).forEach((item) => {
-        let idx = sleepDurationData[item]
-        if (sleepDurationObj[idx]) {
-            sleepDurationObj[idx] += 1
-        } else {
-            sleepDurationObj[idx] = 1
+        Object.keys(data).forEach((item) => {
+            let idx = data[item]
+            if (dataObj[idx]) {
+                dataObj[idx] += 1
+            } else {
+                dataObj[idx] = 1
+            }
+        })
+
+        Object.keys(dataObj).sort().forEach(key => {
+            let val = dataObj[key]
+            dataList.push([key, val])
+        })
+
+        dataList.unshift([title, "Count"])
+
+        const options = {
+            title,
+            curveType: "function",
         }
-    })
 
-    Object.keys(sleepDurationObj).sort().forEach(key => {
-        let val = sleepDurationObj[key]
-        sleepDurationList.push([key, val])
-    })
-
-    sleepDurationList.unshift(["Hours", "Count"])
-
-    const options = {
-        title: "Sleep Duration",
-        curveType: "function",
+        return { dataList, options }
     }
 
+    const sleepDurationList = getDistribution(sleepDurationData, "Duration");
+    const dailyStepsList = getDistribution(dailyStepsData, "Daily Steps");
+    const bpHighList = getDistribution(bpHigh, "High BP")
+    const bpLowList = getDistribution(bpLow, "High BP")
+
+
     return (
-        <Chart
-            chartType="LineChart"
-            width="100%"
-            height="400px"
-            data={sleepDurationList}
-            options={options}
-        />
+        <div>
+            <Chart
+                chartType="LineChart"
+                width="100%"
+                height="400px"
+                data={sleepDurationList.dataList}
+                options={sleepDurationList.options}
+            />
+
+            <Chart
+                chartType="LineChart"
+                width="100%"
+                height="400px"
+                data={dailyStepsList.dataList}
+                options={dailyStepsList.options}
+            />
+
+            <Chart
+                chartType="LineChart"
+                width="100%"
+                height="400px"
+                data={bpHighList.dataList}
+                options={bpHighList.options}
+            />
+
+            <Chart
+                chartType="LineChart"
+                width="100%"
+                height="400px"
+                data={bpLowList.dataList}
+                options={bpLowList.options}
+            />
+        </div>
     );
 }
 
