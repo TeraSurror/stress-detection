871a2e73f431ddcc69e0b3d0959ce91ecc09e681
Harsh Shelar <harshshelar22@gmail.com>
2024-02-04 08:52:10 -0500

    changed name

--- web-client/src/dashboard/index.tsx
+++ web-client/src/dashboard/index.tsx
@@ -39,7 +39,7 @@ const Dashboard: React.FC<Props> = ({ children, changeScreen, activeScreen }) =>
             minWidth: "70%",
             padding: "2em 0",
         }}>
-            <h1 style={{ color: "white" }}>NapNinja.ai - Stress Detection using Sleep Data</h1>
+            <h1 style={{ color: "white" }}>FitFiction - Stress Detection using Sleep Data</h1>
             <p style={{ color: "#58777D" }}>This app predicts the stress levels of a person, by using the Fitbit sleep data</p>
 
             <div style={{
